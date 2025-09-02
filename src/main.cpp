ed4b0cf6801fa2d86f7a3bb85d006e5211eea2f8
alina9221 <alina9221@yandex.ru>
2025-09-02 20:18:31 +0300

    main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,21 @@
 #include <src/matrix.h>
 #include <iostream>
 
+class Matrix {
+private:
+    std::vector<std::vector<int>> data;
+    int rows, cols;
+
+public:
+    // Конструктор
+    Matrix(int r, int c) : rows(r), cols(c) {
+        data.resize(rows, std::vector<int>(cols, 0));
+    }
+ 
+
+
+};    
+
 int main()
 {
     math::Matrix m(3, 3); //Инициализация матрицы m
@@ -13,6 +28,9 @@ int main()
 
     math::Matrix m5(3, 3); 
     m5=m;
+
+    math::Matrix m6(3, 3); 
+    m6=m;
 
     std::cout << "Matrix m is:" << std::endl; //Вывод матрицы m
     m.print();
@@ -40,6 +58,16 @@ int main()
               << "Sum of matrices m5 and m1 is:" << std::endl;
     m5 += m1;
     m5.print();
+
+    std::cout << std::endl //Вычитание m1 и m5
+              << "Subtract of matrices m6 and m1 is:" << std::endl;
+    m6 -= m1;
+    m6.print();
+
+    std::cout << std::endl //Умножение m6 на 5
+              << "Multiplication of matric on 5 is:" << std::endl;
+    m6 *= 5;
+    m6.print();
 
     return 0;
 }
